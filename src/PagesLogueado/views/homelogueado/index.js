05b8958c624c874887051301b7ea6a73fb41f5e4
JA21 <jorgeandres2104@gmail.com>
2020-07-21 05:55:17 -0500

    feat:Realizada vista de menu logueado con style

--- src/PagesLogueado/views/homelogueado/index.js
+++ src/PagesLogueado/views/homelogueado/index.js
@@ -4,12 +4,20 @@ import { Carousel } from 'antd';
 
 import "../../Style/stylehomeL.css";
 
+import Navbar from "../../../components/navBarlogueado";
+
+import { FacebookOutlined, WhatsAppOutlined, InstagramOutlined } from '@ant-design/icons';
+
 function home() {
     return (
         <div className="homegeneral">
-
+            <Navbar />
             <div className="general-text">
-                <p className="text-p">Toorito te permite administrar tus productos, con un manego facil y flexible para que todos los usuarios puedan sacarle la maxima funcionalidad a la app. </p>
+                <div className="text-div">
+                    <p className="text-p">Toorito te permite administrar tus productos,
+                     con un manego facil y flexible para que todos los usuarios puedan sacarle la maxima funcionalidad a la app. </p>
+                </div>
+
                 <Carousel autoplay>
                     <div className="text-carousel">
                         <p className="carousel-p">Res</p>
@@ -22,28 +30,36 @@ function home() {
                     </div>
 
                     <div className="text-carousel">
-                    <p className="carousel-p">Pollo</p>
+                        <p className="carousel-p">Pollo</p>
                         <img
                             src={require("../../../assets/imgs/pollo.jpg")}
                             alt="°Pollo"
                             className="carousel-img"
                         />
-                        
+
                     </div>
 
-                    <div className="text-p">
-                    <p className="carousel-p">Pescado</p>
+                    <div className="text-carousel">
+                        <p className="carousel-p">Pescado</p>
                         <img
                             src={require("../../../assets/imgs/pescado.jpg")}
                             alt="°Pescado"
                             className="carousel-img"
                         />
-                        
+
                     </div>
 
                 </Carousel>
             </div>
-
+            <div className="general-p">
+                <p className="p-p">Para comunicarse con nosotros puede utilizar estas redes sociales,
+                las cuales seran mostradas acontinuación.</p>
+            </div>
+            <div className="general-iconos">
+                <FacebookOutlined className="iconosfacebook" />
+                <WhatsAppOutlined className="iconoswhatsapp" />
+                <InstagramOutlined className="iconosinstagram" />
+            </div>
 
         </div>
     );
